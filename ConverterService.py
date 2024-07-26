552c4dd4b2e78b5837e0c350ab25148e9dd46962
lsrovnal <Lukas.Srovnal.extern@buerse-stuttgart.de>
2024-07-26 22:51:09 +0200

    Initial commit

---
+++ ConverterService.py
@@ -0,0 +1,14 @@
+﻿print("<-- Celcius to Fahrenheit convertor -->")
+
+user_input_celsius = float(input("Enter °C value to convert:"))
+
+
+def celsius_to_fahrenheit(user_input_celsius):
+    f = (9 / 5) * user_input_celsius + 32
+    return f
+
+
+fahrenheit_value = celsius_to_fahrenheit(user_input_celsius)
+
+output = "{} °C is {} °F".format(user_input_celsius, fahrenheit_value)
+print(output)
---
+++ DateTimeService.py
@@ -0,0 +1,35 @@
+﻿from datetime import datetime
+
+# getting the current date and time
+current_datetime = datetime.now()
+
+# getting the year from the current date and time
+current_year = current_datetime.strftime("%Y")
+print("current year = ", current_year)
+
+# getting the month from the current date and time
+current_month = current_datetime.strftime("%m")
+print("current month = ", current_month)
+
+# getting the day/date from the current date and time
+current_day = current_datetime.strftime("%d")
+print("current day = ", current_day)
+
+# getting the time from the current date and time in the given format
+current_time = current_datetime.strftime("%H:%M:%S")
+print("current time = ", current_time)
+
+# getting the date and time from the current date and time in the given format
+current_date_time = current_datetime.strftime("%m/%d/%Y, %H:%M:%S")
+print("current date and time = ", current_date_time)
+
+#Days until end of year.
+day_of_year = current_datetime.timetuple().tm_yday
+print("Today is {} th day of year".format(day_of_year))
+
+
+def count_days_until_end_of_year(day_of_year):
+    return 365 - day_of_year;
+
+
+print("Until end of year remaining {} days.".format(count_days_until_end_of_year(day_of_year)))
