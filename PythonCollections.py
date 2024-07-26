432b4c4809210272b6b23b8a2cc635b0df3a5245
lsrovnal <Lukas.Srovnal.extern@buerse-stuttgart.de>
2024-07-26 23:14:42 +0200

    Python collections

---
+++ PythonCollections.py
@@ -0,0 +1,36 @@
+﻿#Lists
+
+testingList_String = ["Lukas", "Adam", "Radim", "Michal", "Marek"]
+
+#Get last list item
+print(testingList_String[-1])
+
+
+#Check if "name" is present in the list:
+def check_list():
+    name = input("Enter name to check if it's in list: ")
+    if name in testingList_String:
+        print("Yes, {} is in the list".format(name))
+    else:
+        print("No, {} is not in the list".format(name))
+
+
+#Add to list
+testingList_String.append("Martin")
+print(testingList_String)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+#Clear the list
+thislist.clear()
+print(thislist)
+
+
+def myfunc(n):
+    return abs(n - 50)
+
+
+newList = [100, 50, 65, 82, 23]
+newList.sort(key=myfunc)
+print(newList)
